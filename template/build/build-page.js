079465ca6369db2e76eda716d553824707c4af0f
agileago <agileago@gmail.com>
2016-03-17 09:42:42 +0800

    simply build-page

--- template/build/build-page.js
+++ template/build/build-page.js
@@ -2,12 +2,14 @@
  * 需要构建的页面 一次只构建一个页面
  */
 
+var pageName = 'index'
+
 module.exports = {
-  pageName: 'index',
+  pageName: pageName,
   entry: {    // 入口配置
-    app: './src/view/index/main.js'
+    app: './src/view/' + pageName + '/main.js'
   },
-  template: './src/view/index/template.html',  // 页面模板
+  template: './src/view/' + pageName + '/template.html',  // 页面模板
   extractCss: true,           // 是否抽取出css
   absolutePath: false,         // 资源引用路径是否为绝对路径  当项目是部署到根域名下设置此值
   sourceMap: true           //  生产环境下是否需要生成sourceMap
