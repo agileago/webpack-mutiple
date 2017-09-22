e1e2e1c7605534fd2979f6c2f19e1c3b1b993501
agileago <agileago@gmail.com>
2017-09-22 18:23:13 +0800

    fix reload

--- template/build/server.js
+++ template/build/server.js
@@ -7,18 +7,21 @@ const compiler = webpack(config)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath,
-  stats: {
-    colors: true,
-    chunks: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }
+  // stats: {
+  //   colors: true,
+  //   chunks: false,
+  //   children: false,
+  //   chunks: false,
+  //   chunkModules: false
+  // }
+  log: false,
+  heartbeat: 2000
 })
 var hotMiddleware = require('webpack-hot-middleware')(compiler)
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    // console.log(data)
+    // hotMiddleware.publish({ action: 'reload' })
     cb()
   })
 })
